b56b9848b1a76075ac8e1adc0fd0cbf5de0301b6
Lionel Mann <lionelmann@gmail.com>
2014-02-25 18:06:51 -0500

    cpt

--- wordpress/wp-content/themes/magnetics/includes/wp-cuztom-posts/custom-post-product.php
+++ wordpress/wp-content/themes/magnetics/includes/wp-cuztom-posts/custom-post-product.php
@@ -4,10 +4,56 @@ $args = array(
 	'has_archive' => true,
 	'menu_position' => 5,
 	'menu_icon' => 'dashicons-products', //http://melchoyce.github.io/dashicons/
-	'supports'	=> array( 'title', 'editor', 'author', 'thumbnail', 'excerpt' )
+	'supports'	=> array( 'title', 'thumbnail' )
  	);
 
 $product = register_cuztom_post_type( 'Products', $args);
 
+$product->add_meta_box( 
+	'product',
+	'Overview',
+		array(
+			array(
+				'name'          => 'title',
+				'label'         => 'Title',
+				'description'   => '',
+				'type'          => 'text'
+                ),
+			array(
+				'name'          => 'team_description',
+				'label'         => 'Description',
+				'description'   => '',
+				'type'          => 'textarea'
+                )
+		)
+	);
+
+$product->add_meta_box(
+	'product_options',
+	'Options',
+	array(
+		'bundle',    
+			array( 
+				array(
+					'name'          => 'title',
+					'label'         => 'Title',
+					'description'   => '',
+					'type'          => 'text',          
+				),
+				array(
+					'name'          => 'content',
+					'label'         => 'Description',
+					'description'   => '',
+					'type'          => 'WYSIWYG',          
+				),
+				array(
+			        'name'          => 'image',
+			        'label'         => 'Image',
+			        'description'   => '',
+			        'type'          => 'image',
+			    )
+			)
+		)
+	);
 
 ?>
