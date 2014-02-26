43986677cd19d5fd000a3079a1e2f5c7c957caea
Lionel Mann <lionelmann@gmail.com>
2014-02-25 20:23:28 -0500

    brochure cpt

--- wordpress/wp-content/themes/magnetics/functions.php
+++ wordpress/wp-content/themes/magnetics/functions.php
@@ -6,6 +6,7 @@ include('includes/wp-cuztom-helper/cuztom.php');
 //Include post custom posts type. Dependent on /wp-cuztom-helper classes.
 include('includes/wp-cuztom-posts/custom-post-product.php');
 include('includes/wp-cuztom-posts/custom-post-integration.php');
+include('includes/wp-cuztom-posts/custom-post-brochure.php');
 
 //Load custom functions
 require_once('includes/functions/add-classes-to-body.php');
