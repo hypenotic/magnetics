410e05cb7f189e8e893335587b3c5ee1c5e588bf
ajay <ajay786singh@gmail.com>
2014-02-26 18:01:39 +0530

    add video, products landing page and integrations page, fix offcanvas

---
+++ wordpress/wp-content/themes/magnetics/archive-integrations.php
@@ -0,0 +1,36 @@
+<?php
+get_header(); ?>
+
+<?php 
+    $paged  = (get_query_var('paged')) ? get_query_var('paged') : 1;
+    $args   =array(
+    	'post_type' => 'integrations',
+    	'paged' => $paged
+    );
+    query_posts($args);
+?>
+
+<section class="container">  
+    <div class="span-10 center">
+        <h1 class="headline">Integrations</h1>
+        <p class="sub-headline">Lorem Ipsum</p>
+    </div>
+</section>        
+<section class="container content-wrapper">  
+    <div class="span-10 center">
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <div class="block-grid-2">
+            <h4><?php the_title(); ?></h4>
+            <p class="meta"><?php the_excerpt(); ?></p>
+            <a href="<?php the_permalink();?>" class="button"><button class="white">View</button></a>
+        </div>
+        <?php endwhile; endif; ?>
+    </div>
+</section>
+
+<div class="container">
+    <div class="pagi columns-wide">
+        <?php pagination(); ?>
+    </div>
+</div>
+<?php get_footer(); ?>
