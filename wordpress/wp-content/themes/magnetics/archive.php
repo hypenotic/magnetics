998460240f6cd5d16092f96377745e72aab6235f
ajay <ajay786singh@gmail.com>
2014-03-04 13:38:53 +0530

    single article and article archive page update

--- wordpress/wp-content/themes/magnetics/archive.php
+++ wordpress/wp-content/themes/magnetics/archive.php
@@ -1,37 +1,61 @@
 <?php
 /*
-Template Name: Blog Archive
+Template Name: Article & Brochures Archive
 */
 get_header(); ?>
 
 <?php 
     $paged  = (get_query_var('paged')) ? get_query_var('paged') : 1;
     $args   =array(
-    	'post_type' => 'post',
+    	'post_type' => array('post','brochure'),
     	'paged' => $paged
     );
     query_posts($args);
 ?>
 
-<section>  
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <div class="container">
-        <aside>
-            <h6 style="text-transform: uppercase;"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h6>
-            <p class="meta"><?php echo get_the_date(); ?>
-            <br><?php echo get_the_category_list(' / '); ?>
-            </p>
-        </aside>
-        <article>
-            <?php the_excerpt(); ?>
-        </article>       
-    </div>
-    <?php endwhile; endif; ?>
+<section class="page-content">
+	<div class="container">
+        	<div class="span-9 center">
+            	<?php the_title("<h2>","</h2>");?>
+                 <?php 
+					$args = array(
+							'hide_empty' => '0',
+							'orderby' => 'name',
+							'order' => 'ASC',
+							'exclude' => 1
+							);
+				 	$categories=get_categories($args);
+					if($categories) {
+				 ?>
+                
+	             <h3>Select a Category</h3>
+                 	<ul class="filter">
+						<?php foreach($categories as $category) { ?>
+                        <li><a href="#"><?php echo $category->cat_name;?></a></li>
+                        <?php } ?>
+                 	</ul>
+                 <?php } ?>
+	        </div>	       
+    	</div>            
+        <div class="container related">
+        	<?php if(have_posts()): while(have_posts()):the_post();?>
+        	<div class="span-9 center">
+            	<h6>ARTICLE</h6>
+            	<div class="span-5"><h3><?php the_title();?></h3></div>
+            	<div class="span-7">
+                	<p><?php echo get_post_meta(get_the_ID(),'_brochure_content',true);?></p>
+                    <p class="meta inline-meta">
+                         <?php
+                            if(get_post_meta($post->ID,'_brochure_file',true)) {
+                        ?>
+                        <span><a href="<?php echo get_post_meta($post->ID,'_brochure_file',true);?>" target="_blank" class="download"></a></span>
+                        <?php } ?>
+                        <span><a href="<?php the_permalink();?>" class="more"></a></span>
+                    </p>
+                </div>
+                <hr />                
+            </div>
+            <?php endwhile;endif;wp_reset_query();?>
+        </div>
 </section>
-
-<div class="container">
-    <div class="pagi columns-wide">
-        <?php pagination(); ?>
-    </div>
-</div>
 <?php get_footer(); ?>
