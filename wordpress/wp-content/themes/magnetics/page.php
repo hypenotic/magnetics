a0906575ac3c64d48237430bc30a828c88a487ca
ajay <ajay786singh@gmail.com>
2014-03-21 15:42:12 +0530

    404 page update, single product page update, contact page map add in background, few style changes

--- wordpress/wp-content/themes/magnetics/page.php
+++ wordpress/wp-content/themes/magnetics/page.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-
+<?php get_template_part( 'template-part', 'add_video' ); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
     <?php // Get the feature banner image
