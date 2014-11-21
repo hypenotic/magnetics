f34d1fb1ac20958281c31fea0203b54c99d862e8
Jacob Friedman <jacob@subsumo.com>
2014-11-21 11:55:07 -0500

    Text Ratios

--- wordpress/wp-content/themes/magnetics/page.php
+++ wordpress/wp-content/themes/magnetics/page.php
@@ -1,21 +1,11 @@
 <?php get_header(); ?>
-<?php get_template_part( 'template-part', 'add_video' ); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-    <?php // Get the feature banner image
-        if (has_post_thumbnail()) {
-            $image_id = get_post_thumbnail_id();
-            $image_url = wp_get_attachment_image_src($image_id,'full', true);
-		}
-    ?>        
-    <div class="page-title">
-            <div class="container">
-                <section class="span-10-center">
-                    <?php the_title( '<h1 class="headline">', '</h1>' ); ?>
-                    <?php get_template_part( 'template-part', 'add_sub_heading' ); ?>
-	            </section>
-            </div>    
-    </div>     
+  <article>
+      <h1><?php the_title(); ?></h1>
+      <?php the_content(); ?>
+  </article>
+
     <div class="page-content">  	
             <div class="container">
                 <section class="span-10-center">
