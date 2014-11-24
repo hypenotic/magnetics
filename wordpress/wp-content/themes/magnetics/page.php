e62e55a20378c608034b6a94747a665456717145
Jacob Friedman <jacob@subsumo.com>
2014-11-24 13:55:41 -0500

    annoying tabs problem on page

--- wordpress/wp-content/themes/magnetics/page.php
+++ wordpress/wp-content/themes/magnetics/page.php
@@ -1,21 +1,58 @@
 <?php get_header(); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
+<section>
   <article>
-      <h1><?php the_title(); ?></h1>
-      <?php the_content(); ?>
+    <h1><?php the_title(); ?></h1>
+    <?php the_content(); ?>
   </article>
+</section>
+<?php global $post; ?>
+<?php print_r(get_post_meta($post->ID, 'Page Tabs', true)); ?>
+              
 
-    <div class="page-content">  	
-            <div class="container">
-                <section class="span-10-center">
-                    <?php the_content(); ?>
-                </section>
-                <?php 
-					$tabs = get_pages( array( 'child_of' => $post->ID, 'sort_column' => 'post_date', 'sort_order' => 'desc' ) ); 
-					if($tabs) {
-				?>
-                <section class="span-10-center">
+<?php echo get_post_meta(get_the_ID(),'_tabs_tabLeft',true); ?>
+                            
+
+<?php print_r(get_post_meta(get_the_ID(), 'page_tabs', true)); ?> 
+<?php print_r(get_post_meta(get_the_ID(), '_page_tabs', true)); ?> 
+<?php print_r(get_post_meta(get_the_ID(), '_page_tabs_tabs_tabLeft', true)); ?> 
+
+
+
+<?php /*
+   array(
+                    'name'          => 'tabLeft',
+                    'label'         => 'Overview Text',
+                    'description'   => '',
+                    'type'          => 'wysiwyg'
+                ),
+                array(
+                    'name'          => 'tabLeftTextArea',
+                    'label'         => 'Content',
+                    'description'   => '',
+                    'type'          => 'wysiwyg'
+                )
+            ),
+
+            'Right Tab' => array(
+                array(
+                    'name'          => 'tabRight',
+                    'label'         => 'Overview Text',
+                    'description'   => '',
+                    'type'          => 'wysiwyg'
+                ),
+                array(
+                    'name'          => 'tabRightTextArea',
+                    'label'         => 'Content',
+                    'description'   => '',
+                    'type'          => 'wysiwyg'
+                ) 
+
+                
+
+
+
+
                 	<div class="tabs">
                     	<ul class="tab-header">
                        		<?php 
@@ -43,10 +80,9 @@
                             <?php $j++; } ?>
                         </div>
                     </div>
-                </section>
-                <?php } ?>
-            </div>
-    </div>
+
+                    */ ?>
+
 
 <?php endwhile; endif; ?>
 <?php get_footer(); ?>
