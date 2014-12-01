ffe95ad91ad90266f13444732e2faae090bd8e3f
Jacob Friedman <jacob@subsumo.com>
2014-12-01 11:07:44 -0500

    Merged

--- wordpress/wp-content/themes/magnetics/page.php
+++ wordpress/wp-content/themes/magnetics/page.php
@@ -1,21 +1,29 @@
 <?php get_header(); ?>
+
+<!-- Get custom meta values -->
+<?php
+	$tabLeft 	= get_post_meta(get_the_ID(), '_page_tabs_tableft', true);
+	$tabRight 	= get_post_meta(get_the_ID(), '_page_tabs_tabright', true);
+?>
+
+<!-- Start loop -->
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
 <section>
-  <article>
-    <h1><?php the_title(); ?></h1>
-    <?php the_content(); ?>
-  </article>
+ 	<article>
+    	<h1><?php the_title(); ?></h1>
+    	<?php the_content(); ?>
+  	</article>
 </section>
 
-<?php 
+<!-- tabLeft AND tabRight have data --> 
+<?php if ($tabLeft && $tabRight) { ?>
 
-$metaTabs = get_post_meta(get_the_ID(), '_page_tabs_tableft', true) && get_post_meta(get_the_ID(), '_page_tabs_tabright', true);
+    <?php get_template_part( 'template', 'tabsTwo' ); ?>
 
-if($metaTabs) { 
+<?php } ?>
 
-	get_template_part( 'template', 'tabsTwo' ); 
-
-} ?>
-
+<!-- End loop -->
 <?php endwhile; endif; ?>
+
 <?php get_footer(); ?>
