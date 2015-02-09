44057be39eca8737dc965af7adb78ea0922f41ae
Lionel Mann <lionelmann@gmail.com>
2015-02-08 21:55:34 -0500

    removed bugherd code

--- wordpress/wp-content/themes/magnetics/header.php
+++ wordpress/wp-content/themes/magnetics/header.php
@@ -23,17 +23,6 @@
 
 <?php wp_head(); ?>
 
-<script type='text/javascript'>
-(function (d, t) {
-  var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
-  bh.type = 'text/javascript';
-  bh.src = '//www.bugherd.com/sidebarv2.js?apikey=1wpsnobnz9aehqcpybypga';
-  s.parentNode.insertBefore(bh, s);
-  })(document, 'script');
-</script>
-
-
-
 </head>
 
 <body <?php body_class();?>>
