2e739980d43c4d9c9bba00a7ddef9b9403f1c8c5
Jacob Friedman <jacob@Jacobs-MacBook-Pro.local>
2015-05-05 11:05:02 -0400

    rma link

--- wordpress/wp-content/themes/magnetics/module-rmaformOverview.php
+++ wordpress/wp-content/themes/magnetics/module-rmaformOverview.php
@@ -1,3 +1,3 @@
         <h3>RMA FORM</h3>
             <p>The following form will provide you with the information and instructions for the return of merchandise purchased from us for examination, credit, or exchange.</p>
-            <p><a href="<?php bloginfo('template_url') ?>/rma-form">Get Started ></a></p>
+            <p><a href="<?php bloginfo('url') ?>/rma-form">Get Started ></a></p>
