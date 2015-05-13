4a7f68b3cd283b1a4ddaa9d8fbed67830838ebe1
Jacob Friedman <jacob@subsumo.com>
2015-05-13 13:59:07 -0400

    update menu

--- wordpress/wp-content/themes/magnetics/js/build/menu-function.js
+++ wordpress/wp-content/themes/magnetics/js/build/menu-function.js
@@ -1,10 +1,12 @@
 jQuery(document).ready(function() {
 	jQuery('#nav-toggle').bigSlide({'side':'right'});
 
-	jQuery(window).on('scroll', function(jQuery){
+	jQuery(window).on('scroll', function() {
 
 	  if(jQuery(window).scrollTop() !== 0) {
-	    jQuery('nav#toggler').toggleClass('transparent');
+	    jQuery('nav#toggler:not(.transparent)').addClass('transparent');
+	  } else {
+	  	jQuery('nav#toggler').removeClass('transparent');
 	  }
 
 	});
