961d9e2dc1dd6c6dbe657982e2bd054b6eb79f56
ajay <ajay786singh@gmail.com>
2015-08-20 18:17:44 +0530

    fix #245

--- wordpress/wp-content/themes/magnetics/module-tabsProduct.php
+++ wordpress/wp-content/themes/magnetics/module-tabsProduct.php
@@ -1,10 +1,11 @@
 <?php 
     // Custom meta values 
-    $metaWhatsInTheBox = get_post_meta(get_the_ID(), '_product_tabs_whats_in_the_box', true);
-    $metaAdditionalOptions = get_post_meta(get_the_ID(), '_additional_options', true);
-    $metaIntegrations = get_post_meta(get_the_ID(), '_product_tabs_post_meta_integrations', true);
-    $metaPlaysWellWith = get_post_meta(get_the_ID(), '_product_tabs_plays_well_with', true);
-    $metaSystemAtAGlance = get_post_meta(get_the_ID(), '_product_tabs_system_at_a_glance', true);
+	$postID=get_the_ID();
+    $metaWhatsInTheBox = get_post_meta($postID, '_product_tabs_whats_in_the_box', true);
+    $metaAdditionalOptions = get_post_meta($postID, '_additional_options', true);
+    $metaIntegrations = get_post_meta($postID, '_product_tabs_post_meta_integrations', true);
+    $metaPlaysWellWith = get_post_meta($postID, '_product_tabs_plays_well_with', true);
+    $metaSystemAtAGlance = get_post_meta($postID, '_product_tabs_system_at_a_glance', true);
 
 
     if($metaWhatsInTheBox || (isset($metaAdditionalOptions[0]['_title']) && ($metaAdditionalOptions[0]['_title'] !== '')) || ($metaIntegrations[0] != 0) || ($metaSystemAtAGlance[0] != 0)) {
@@ -45,94 +46,32 @@
         <br style="clear:both" />
 
         <div class="resp-tabs-container">
-
-             <?php
-                if($metaWhatsInTheBox) { ?>
-                <div id="whatsinthebox" aria-controls="whatsinthebox">
-                        <?php echo $metaWhatsInTheBox; ?>  
-                </div>
-            <?php } ?>
-            <!-- end #metawhatsinthebox -->
-
-            <?php
-           if(isset($metaAdditionalOptions[0]['_title']) && ($metaAdditionalOptions[0]['_title'] !== ''))  { ?>
-           <div id="additionaloptions">
-                    
-                <?php foreach ( $metaAdditionalOptions as $option ) { ?>
-                <h3 id="<?php sanitize_title($option['_title']); ?>"><?php echo $option['_title']; ?></h3>
-                <p><?php echo $option['_description']; ?></p>
-                <!-- End Loop -->
-                <?php } ?>
-
-            </div>
-
-           <?php  } ?>
-           <!-- end #additionaloptions -->
-
-
-            <?php if($metaIntegrations[0] != 0)  { ?>
-            <div id="integrations"> 
-            <?php
-
-            if($metaIntegrations) {
-                $args = array(
-                    'post__in'    =>  $metaIntegrations
-                );
-
-            $related_posts = get_posts($args);
-
-             foreach ( $related_posts as $post ) { 
-
-                $metaBannerSubheading = get_post_meta($post->ID, '_banner_subheading', true);
-         
-                ?>
-                <h3><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h3>
-                <p><?php echo $metaBannerSubheading; ?></p>
-            <!-- End Loop -->
-            <?php }
-            }
-            wp_reset_postdata();?>
-
-            <br style="clear:both" />
-
-
-            </div>
-            <?php } ?>
-            <!-- end integrations -->
-
-<?php if($metaSystemAtAGlance[0] != 0) { ?>
-            <div id="systemataglance">
-
-                <?php get_template_part( 'template', 'brochureFileOptions' ); ?>
-                
-                <h3>The <?php the_title() ?> comes with:</h3>
-
-                <section class="images">
-            
-            <?php 
-
-             if($metaSystemAtAGlance) {
-                    foreach($metaSystemAtAGlance as $image) { ?>
-
-                    <?php if ($image) {
-                        echo wp_get_attachment_image($image, 'full', 'class=item');
-                        }
-                    ?>
-
-                <?php }
-             }
-            ?>
+			<!-- Start System At a Glance -->
+			<?php if($metaSystemAtAGlance[0] != 0) { ?>
+				<div id="systemataglance">
+					<?php get_template_part( 'template', 'brochureFileOptions' ); ?>
+					<h3>The <?php the_title() ?> comes with:</h3>
+					<section class="images">
+						<?php 
+						 if($metaSystemAtAGlance) {
+								foreach($metaSystemAtAGlance as $image) { ?>
+								<?php 
+									if ($image) {
+										echo wp_get_attachment_image($image, 'full', 'class=item');
+									}
+								?>
+						<?php 	}
+						 }
+						?>
                 </section>
-
                  <footer>
                     <ul>
                     <li>Additional Options</li>
-
                     <?php
                         if($metaAdditionalOptions) {
-                    
+              
                         foreach ( $metaAdditionalOptions as $option ) { ?>
-                <li><?php echo $option['_title']; ?></li>
+						<li><?php echo $option['_title']; ?></li>
             <!-- End Loop -->
             <?php }
 
@@ -190,6 +129,60 @@
             </div>
 
             <?php } ?>
+			<!-- End System At a Glance -->
+             <?php
+                if($metaWhatsInTheBox) { ?>
+                <div id="whatsinthebox" aria-controls="whatsinthebox">
+                        <?php echo $metaWhatsInTheBox; ?>  
+                </div>
+            <?php } ?>
+            <!-- end #metawhatsinthebox -->
+
+            <?php
+           if(isset($metaAdditionalOptions[0]['_title']) && ($metaAdditionalOptions[0]['_title'] !== ''))  { ?>
+           <div id="additionaloptions">
+                    
+                <?php foreach ( $metaAdditionalOptions as $option ) { ?>
+                <h3 id="<?php sanitize_title($option['_title']); ?>"><?php echo $option['_title']; ?></h3>
+                <p><?php echo $option['_description']; ?></p>
+                <!-- End Loop -->
+                <?php } ?>
+
+            </div>
+
+           <?php  } ?>
+           <!-- end #additionaloptions -->
+
+
+            <?php if($metaIntegrations[0] != 0)  { ?>
+            <div id="integrations"> 
+            <?php
+
+            if($metaIntegrations) {
+                $args = array(
+                    'post__in'    =>  $metaIntegrations
+                );
+
+            $related_posts = get_posts($args);
+
+             foreach ( $related_posts as $post ) { 
+
+                $metaBannerSubheading = get_post_meta($post->ID, '_banner_subheading', true);
+         
+                ?>
+                <h3><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h3>
+                <p><?php echo $metaBannerSubheading; ?></p>
+            <!-- End Loop -->
+            <?php }
+            }
+            wp_reset_postdata();?>
+
+            <br style="clear:both" />
+
+
+            </div>
+            <?php } ?>
+            <!-- end integrations -->
         </div>
 </section>
 
