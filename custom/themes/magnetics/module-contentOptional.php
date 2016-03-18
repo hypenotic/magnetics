91a78cf5ac2a1b78d5d0fdc62974b561bdaf1666
Emily Dela Cruz <delacruz.emily@gmail.com>
2016-03-18 09:49:28 -0400

    Update: fixes #284 - media kit section

--- custom/themes/magnetics/module-contentOptional.php
+++ custom/themes/magnetics/module-contentOptional.php
@@ -2,11 +2,16 @@
     // Custom meta values 
     $metaContentBlockText = get_post_meta(get_the_ID(), '_content_block_1_text', true);
 
+    $title = get_the_title();
+    $prehash = preg_replace("/[^a-zA-Z]/", "", $title);
+    $lowercase = strtolower($prehash);
+    $hash = $lowercase;	
+
     if($metaContentBlockText) {
 
 ?>
 
-<section class="optional">
+<section class="optional <?php echo $hash; ?>">
 
     <?php echo $metaContentBlockText; ?>
 
