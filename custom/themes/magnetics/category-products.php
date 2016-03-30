66f2045b447a18ea5237dded92a619bf003f1a45
Emily Dela Cruz <delacruz.emily@gmail.com>
2016-03-30 12:57:11 -0400

    Update: fixes #293 - product order

--- custom/themes/magnetics/category-products.php
+++ custom/themes/magnetics/category-products.php
@@ -23,14 +23,21 @@
     <section class="products">
 
     <?php
-    $parentCatName = single_cat_title('',false);
-    $parentCatID = get_cat_ID($parentCatName);
-    $childCats = get_categories( 'child_of='.$parentCatID );
-    if(is_array($childCats)):
-    foreach($childCats as $category){ ?>
+    $parentCatName  = single_cat_title('',false);
+    $parentCatID    = get_cat_ID($parentCatName);
+    // Use the category's description field to order
+    $args           = array (
+        'orderby'   => 'description',
+        'include'   => array(6,5,4,22,11,23),
+    );
+    $wpcat          = get_terms( 'category', $args );
+    // $childCats      = get_categories( 'child_of='.$parentCatID );
+
+    if(is_array($wpcat)):
+    foreach($wpcat as $category){ ?>
 
 
-    <label class="<?php echo $category->slug; ?>"><span><?php echo $category->cat_name; ?></span></label>
+    <label class="<?php echo $category->slug; ?>"><span><?php echo $category->name; ?></span></label>
     <ul>
     <?php query_posts('cat='.$category->term_id);
     while(have_posts()): the_post(); $do_not_duplicate = $post->ID; ?>
@@ -73,61 +80,7 @@
     ?>
     
 
-
-    <?php
-
-    // integrations
-
-    $parentCatID = get_category_by_slug('product-integrations');
-    $category = get_category($parentCatID); 
-
-    ?>
-
-
-
-    <label class="<?php echo $category->slug; ?>"><span><?php echo $category->cat_name; ?></span></label>
-    <ul>
-    <?php query_posts('cat='.$category->term_id);
-    while(have_posts()): the_post(); $do_not_duplicate = $post->ID; ?>
-    <!-- POST CODE -->
-
-    <li class="product <?php echo $category->slug; ?>">
-
-<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-     <h3><?php the_title(); ?></h3>
-
-        <?php 
-        // Banner Subheading
-        // Custom meta values 
-        $metaBannerSubheading = get_post_meta($post->ID, '_banner_subheading', true);
-
-        if($metaBannerSubheading !== -1) { ?>
-            <p><?php echo $metaBannerSubheading; ?>
-
-             
-            </p>
-
-            <!--
-                <a class="button clear" href="<?php the_permalink(); ?>" title="Learn More">Learn More</a>
-            -->
-
-        <?php } ?>
-
-        </a>
-       
-
-    </li>
-
-    <!-- END POST CODE -->
-    <?php
-    endwhile; 
-    ?>
-    </ul>
-    <?php
-    wp_reset_query(); 
-    ?>
-
-</section>
+    </section>
 
 </section>
 
