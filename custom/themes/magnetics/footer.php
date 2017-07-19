d2a947b1f33a56036c32dca092b3de707b45720e
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-07-19 11:28:50 -0400

    Updates tabs and footer

--- custom/themes/magnetics/footer.php
+++ custom/themes/magnetics/footer.php
@@ -16,15 +16,14 @@
 
         <section>
             <h3>Contact</h3>
-                <h4>Marine Magnetics Corp.</h4>
-                <br /> <br />
+                <h4 style="font-weight: 400;">Marine Magnetics Corp.</h4>
                 <p>
                     135 SPY Court <br />
                     Markham, Ontario <br />
                     L3R 5H6  Canada<br />
                 </p>
                 <p><a href="mailto:info@marinemagnetics.com">info@marinemagnetics.com</a></p>
-                <p>Tel. +1 (905) 479-9727</p>
+                <p style="margin-bottom: 0px">Tel. +1 (905) 479-9727</p>
                 <p>Fax. +1 (905) 479-9484</p>
         </section>
     </footer>
