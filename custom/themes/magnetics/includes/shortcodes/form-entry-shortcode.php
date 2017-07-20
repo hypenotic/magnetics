6bb832a230c90a5dbc68c2ca07d9962ed92e2268
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-07-20 13:00:38 -0400

    Updates GF shortcode

--- custom/themes/magnetics/includes/shortcodes/form-entry-shortcode.php
+++ custom/themes/magnetics/includes/shortcodes/form-entry-shortcode.php
@@ -8,4 +8,12 @@ function gravity_form_entry_number(){
 }
 add_shortcode( 'rmaNumber', 'gravity_form_entry_number' );
 
+function gravity_form_entry_date(){
+
+	//echo $_GET["rmaNumber"];
+	$rmaDate = $_GET["rmaDate"];
+	return $rmaDate;
+}
+add_shortcode( 'rmaDate', 'gravity_form_entry_date' );
+
 ?>
