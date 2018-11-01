cc1ed38d83885e96eeb383d201ec2661d711300e
Emily Dela Cruz <delacruz.emily@gmail.com>
2018-11-01 12:26:05 -0400

    Update rma form styles

--- custom/themes/magnetics/includes/functions/enqueue-style.php
+++ custom/themes/magnetics/includes/functions/enqueue-style.php
@@ -6,7 +6,7 @@
 
 function my_styles() {
 	if(!is_admin()) {
-		wp_register_style('style', get_template_directory_uri() . '/style.css?v=1.1', '1.3');
+		wp_register_style('style', get_template_directory_uri() . '/style.css?v=181101-59fe3ni49', '1.3');
 		wp_enqueue_style( 'style' );
 	}
 }
