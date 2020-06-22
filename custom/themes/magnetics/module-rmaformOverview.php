e224dccfdcc90fba0d2bec69a0d358e65f0067d8
lionelmann <lionelmann@gmail.com>
2020-06-22 13:04:15 -0400

    missing verified vendor logo

--- custom/themes/magnetics/module-rmaformOverview.php
+++ custom/themes/magnetics/module-rmaformOverview.php
@@ -2,6 +2,6 @@
 <p>The following form will provide you with the information and instructions for the return of merchandise purchased from us for examination, credit, or exchange.</p>
 <p><a href="<?php bloginfo('url') ?>/rma-form">Get Started ></a></p>
 &nbsp;
-<p style="text-align: left;"><a href="https://uscontractorregistration.com" target="_blank" rel="noopener"><img title="US Federal Contractor Registration System for Award Management Verified Vendor Seal" src="https://uscontractorregistration.com/images/Verified-Vendor-2019-2020-sm.png" alt="USFCR Verified Vendor" /></a></p>
+<p style="text-align: left;"><a href="https://uscontractorregistration.com" target="_blank" rel="noopener"><img title="US Federal Contractor Registration System for Award Management Verified Vendor Seal" src="<?php echo get_bloginfo('template_url')?>/dist/images/Verified-Vendor-2019-2020-sm.png" alt="USFCR Verified Vendor" /></a></p>
 
             
